b6f8d8e578fdad8d16d80146fd657ef14aa6a94a
RamX23 <112005895+RamX23@users.noreply.github.com>
2024-12-12 10:35:05 +0530

    Update createToken.js

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -16,7 +16,7 @@ const generateToken = (res, userId) => {
     sameSite: 'None',
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
   });
-
+  console.log(token);
   return token;
 };
 
