ae83caa8dcdca070e088be36c5443380ce370d70
RamX23 <112005895+RamX23@users.noreply.github.com>
2024-12-12 11:35:33 +0530

    Update index.js for cors

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ connctdb();
 
 
 const app=express();
-app.use(cors());
+app.use(cors(),credentials:true);
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
