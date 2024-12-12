a04c1c84e1740fa4d2674ac6ac6ac1141c2169e1
RamX23 <112005895+RamX23@users.noreply.github.com>
2024-12-12 09:54:04 +0530

    Update userRoutes.js
    Updated register bug

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,6 +5,7 @@ const router=express.Router()
 
 router.route("/").post(createdUser).get(authenticate,authorizeadmin,getAllUsers);
 router.post('/auth',loginUser);
+router.post('/register',createdUser);
 router.post('/logout',logoutUser);
 router.route('/profile').get(authenticate,getUserProfile).put(authenticate,updateUserProfile)
 router.route('/:id').delete(authenticate,authorizeadmin,deleteUserById).get(authenticate,getUserById).put(authenticate,authorizeadmin,updateUserById);
