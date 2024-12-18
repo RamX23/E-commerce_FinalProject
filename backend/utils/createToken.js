24c1281a7e4e0812d5c68996790a915ad67130f8
RamX23 <112005895+RamX23@users.noreply.github.com>
2024-12-18 14:44:58 +0530

    Update createToken.js

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -1,28 +1,20 @@
-import jwt from 'jsonwebtoken';
-
+  import jwt from 'jsonwebtoken';
+ let JWT_SECRET='abac12afsakjlaaf'
 const generateToken = (res, userId) => {
   if (!process.env.JWT_SECRET) {
     throw new Error('JWT_SECRET is not defined');
   }
 
-  const isSecure = process.env.NODE_ENV === 'production';
-  
-  // Generate JWT token
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: '30d', // Token expiration in 30 days
+    expiresIn: '30d',
   });
 
-  // Set the token in a secure, httpOnly cookie
   res.cookie('jwt', token, {
-    httpOnly: true, 
-    sameSite: 'None',
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development',
+    sameSite: 'strict',
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
   });
-
-  // Optional: Only log the token in development mode
- 
-    console.log(token);
-  
 
   return token;
 };
