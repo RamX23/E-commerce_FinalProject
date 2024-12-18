573928034be83bd54febc981de4332e8f63f49bd
RamX23 <112005895+RamX23@users.noreply.github.com>
2024-12-18 16:00:34 +0530

    Update createToken.js set nodeenv to productn

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -11,7 +11,7 @@ const generateToken = (res, userId) => {
 
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'development',
+    secure: process.env.NODE_ENV === 'production',
     sameSite: 'Strict',
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
   });
