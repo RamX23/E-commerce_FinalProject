ee3cd560b748e3b53fa534561b4edaa0489b4e9d
RamX23 <112005895+RamX23@users.noreply.github.com>
2025-02-25 12:47:38 +0530

    Update uploadRoutes.js

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,50 +1,44 @@
-import path from "path";
-import express from "express";
-import multer from "multer";
+import express from 'express';
+import multer from 'multer';
+import cloudinary from '../utils/cloudinary.js';  
+import streamifier from 'streamifier'; 
 
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
 
-  filename: (req, file, cb) => {
-    const extname = path.extname(file.originalname);
-    cb(null, `${file.fieldname}-${Date.now()}${extname}`);
-  },
-});
+const storage = multer.memoryStorage(); 
+const upload = multer({ storage });
 
-const fileFilter = (req, file, cb) => {
-  const filetypes = /jpe?g|png|webp/;
-  const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
-
-  const extname = path.extname(file.originalname).toLowerCase();
-  const mimetype = file.mimetype;
-
-  if (filetypes.test(extname) && mimetypes.test(mimetype)) {
-    cb(null, true);
-  } else {
-    cb(new Error("Images only"), false);
+router.post('/', upload.single('image'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send({ message: 'No image file provided' });
   }
-};
 
-const upload = multer({ storage, fileFilter });
-const uploadSingleImage = upload.single("image");
-
-router.post("/", (req, res) => {
-  uploadSingleImage(req, res, (err) => {
-    if (err) {
-      res.status(400).send({ message: err.message });
-    } else if (req.file) {
-      res.status(200).send({
-        message: "Image uploaded successfully",
-        image: `/${req.file.path}`,
-      });
-    } else {
-      res.status(400).send({ message: "No image file provided" });
+const uploadStream = cloudinary.uploader.upload_stream(
+  {
+    resource_type: "auto",
+    eager: [
+      { width: 500, height: 500, crop: "fill" }, 
+      { width: 1000, height: 1000, crop: "limit" },
+    ],
+    folder: "products", 
+    quality: "auto", 
+    fetch_format: "auto", 
+  },
+  (error, result) => {
+    if (error) {
+      return res.status(500).send({ message: "Error uploading to Cloudinary", error });
     }
-  });
+
+    res.status(200).send({
+      message: "Image uploaded successfully",
+      image: result.secure_url, 
+    });
+  }
+);
+
+
+  streamifier.createReadStream(req.file.buffer).pipe(uploadStream);
 });
 
 export default router;
