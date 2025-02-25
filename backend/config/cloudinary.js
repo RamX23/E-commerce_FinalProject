db3520eac26989b5ce80d920356455f61f072dce
RamX23 <112005895+RamX23@users.noreply.github.com>
2025-02-25 13:06:19 +0530

    Add files via upload

---
+++ backend/config/cloudinary.js
@@ -0,0 +1,12 @@
+// cloudinaryConfig.js
+import { v2 as cloudinary } from 'cloudinary';
+
+
+cloudinary.config({
+  cloud_name: 'dfqdkethh',
+  api_key:844458568537763,
+  api_secret: 'v9Mza7uRWa-59m9pdTHLE0PQxbg', 
+});
+
+export default cloudinary;
+
